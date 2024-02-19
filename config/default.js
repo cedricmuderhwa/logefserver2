b4467399c795bdd0519ab12b78c4cf3e38ee3738
cedricmuderhwa <cedriclf2@gmail.com>
2024-02-19 03:41:39 +0100

    Update default.js

--- config/default.js
+++ config/default.js
@@ -1,8 +1,8 @@
 module.exports = {
     port: 1338,
-    origin: 'http://localhost:3000',
+    origin: 'https://logef.vercel.app',
     dbUri: "mongodb+srv://cedriclf2:Mle251991S@cluster0.9imrqth.mongodb.net/?retryWrites=true&w=majority",
-    socketHost: 'localhost',
+    socketHost: 'https://logef.vercel.app',
     saltWorkFactor: 10,
     accessTokenTtl:"45m",
     refreshTokenTtl: "1y",
